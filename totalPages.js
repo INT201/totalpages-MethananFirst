5807b4612c16b6334ae129028de04f762b4c25c0
Methanan <matanun.first@mail.kmutt.ac.th>
2022-09-08 22:39:09 +0700

    toatal

--- totalPages.js
+++ totalPages.js
@@ -2,5 +2,13 @@ const { template } = require('@babel/core')
 
 function totalPages(arrayItems, rowsPerPage) {
   //code here
+  if (arrayItems === null || arrayItems === undefined) {
+    return undefined;
+  } else if (rowsPerPage === null || rowsPerPage === undefined || rowsPerPage == 0) {
+    return 1;
+  } else {
+    let R = arrayItems.length/rowsPerPage
+    return Math.ceil(R);
+  }
 }
 module.exports = totalPages
